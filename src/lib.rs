b9adc84c6f62067b33e46b5d6fceaf9adc073476
Kim Rutherford <kmr@aska.gen.nz>
2025-02-20 01:17:38 +1300

    Add correct value for "property-label"
    Refs pombase/website#2331

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,30 @@ use std::{collections::HashMap, io::{BufReader, Read}};
 
 extern crate serde_json;
 #[macro_use] extern crate serde_derive;
+use phf::phf_map;
 
 use anyhow::Result;
+
+static REL_NAMES: phf::Map<&'static str, &'static str> = phf_map! {
+    "BFO:0000050" => "part of",
+    "BFO:0000051" => "has part",
+    "RO:0002233" => "has input",
+    "RO:0002234" => "has output",
+    "RO:0002333" => "enabled by",
+    "RO:0002413" => "directly provides input for",
+    "BFO:0000066" => "occurs in",
+    "RO:0001025" => "located in",
+    "RO:0002092" => "happens during",
+    "RO:0002407" => "indirectly activates",
+    "RO:0002409" => "indirectly inhibits",
+    "RO:0002629" => "directly positively regulates",
+    "RO:0002630" => "directly negatively regulates",
+    "RO:0002304" => "causally upstream of, positive effect",
+    "RO:0002305" => "causally upstream of, negative effect",
+    "RO:0012005" => "is small molecule activator of",
+    "RO:0012006" => "is small molecule inhibitor of",
+    "RO:0012009" => "constitutively upstream of",
+};
 
 type ModelId = String;
 type FactId = String;
@@ -119,7 +141,12 @@ pub fn parse(source: &mut dyn Read) -> Result<NoctuaModel> {
     let mut fact_map = HashMap::new();
     let mut individual_map = HashMap::new();
 
-    for fact in raw_model.facts.into_iter() {
+    for mut fact in raw_model.facts.into_iter() {
+         if fact.property_label == fact.property {
+             if let Some(&rel_name) = REL_NAMES.get(&fact.property) {
+                 fact.property_label = rel_name.to_owned();
+             }
+         }
         fact_map.insert(fact.id(), fact);
     }
 
@@ -149,11 +176,13 @@ mod tests {
         assert!(model.facts().count() == 42);
         assert!(model.individuals().count() == 82);
 
-        let individual1_id =
-            &model.facts().next().unwrap().object;
+        let first_fact = model.facts().next().unwrap();
+        assert_ne!(first_fact.property, first_fact.property_label);
+
+        let individual1_id = &first_fact.object;
 
         let lookup_individual1 = model.get_individual(individual1_id).unwrap();
 
-        assert!(*individual1_id == lookup_individual1.id);
+        assert_eq!(*individual1_id, lookup_individual1.id);
     }
 }
